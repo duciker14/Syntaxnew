2437150b6fdc2f3dd52bc173c5aed13edadaa0ae
duciker14 <duciker14@gmail.com>
2021-04-22 13:48:35 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Systax
+# Syntax
---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,34 @@
+package basic.dev;
+/**
+ * Ghi chu: comment nhieu dong
+ * @author LAPTOP KIM ANH
+ * 1. Ten lop trung ten file
+ * 2. Ten lop chu cai dau viet hoa, neu nhu tu ghep thi cac chu cai dau viet hoa
+ * 3. Ten lop dat phai co y nghia
+ */
+
+public class MainApp {
+//	comment 1 dong cach 1. phim nong Ctrl 
+/* comment 1 dong cach 2*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        /**
+         * Cach dat ten bien
+         * 1. Khong chua cac ky tu dac biet
+         * 2. Khong chua khoang trang
+         * 3.viet thuong
+         * 4. khong duoc bat dau bang so		
+         */
+		int number;
+		String nameSchool;
+	}
+	/*
+	 * Ten phuong thuc tuan theo quy tac camelCase
+	 */
+	public void show() {
+		
+	}
+		
+
+
+}
